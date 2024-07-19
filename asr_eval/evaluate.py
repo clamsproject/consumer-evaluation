fcab8ba019524e8745efb171d48dc77e898beb7a
Zixin Jiang <147675149+1192119703jzx@users.noreply.github.com>
2024-07-19 12:38:49 -0400

    fix small typo in evaluate.py

--- asr_eval/evaluate.py
+++ asr_eval/evaluate.py
@@ -9,7 +9,7 @@ from mmif import Mmif, DocumentTypes
 # constant:
 ## note that this repository is a private one and the files are not available to the public (due to IP concerns)
 ## hence using goldretriever to download the gold files WILL NOT work (goldretreiver is only for public repositories)
-GOLD_URL = "https://github.com/clamsproject/aapb-collaboration/tree/master/21"
+GOLD_URL = "https://github.com/clamsproject/aapb-collaboration/tree/89b8b123abbd4a9a67c525cc480173b52e0d05f0/21"
 
 
 def get_text_from_mmif(mmif):
@@ -71,7 +71,7 @@ def batch_run_wer(hyp_dir, gold_dir):
             except Exception as wer_exception:
                 print("Error processing file: ", hyp_file.name, wer_exception)
 
-    with open(f'old_results@{hyp_dir.name}.csv', 'w') as fp:
+    with open(f'results@{hyp_dir.name}.csv', 'w') as fp:
         fp.write('GUID,WER-case-sensitive,WER-case-insens\n')
         werS_sum = 0
         werI_sum = 0
