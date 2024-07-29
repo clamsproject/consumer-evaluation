df74628df217f0461809076880518dd4178a3c0d
Bohan Jiang <bohanj.cd@gmail.com>
2024-07-29 16:39:47 -0400

    Added log feature enabling export debug messages

--- rfb_eval/evaluate_alt.py
+++ rfb_eval/evaluate_alt.py
@@ -11,10 +11,11 @@ from typing import Dict, Set, Optional, Tuple, Union, List
 from collections import defaultdict
 from io import StringIO
 from argparse import ArgumentParser, Namespace
+import logging
 
 from mmif import Mmif, AnnotationTypes, View, Annotation
 from mmif.utils import video_document_helper as vdh
-from clams_utils.aapb import guidhandler
+from clams_utils.aapb import guidhandler, goldretriever #TODO: goldretriver will be used after the URL for the gold-standard data is available
 
 import pandas as pd
 
@@ -27,33 +28,39 @@ RFB_APP = 'http://apps.clams.ai/role-filler-binder/41cb5b8'
 def get_adj_aligned_ann(ann: Annotation, view: View) -> Optional[Annotation]:
     """
     Get the aligned annotation residing in adjacent view
-    
+
     :param ann: any type of annotation
     :param view: the view that contains both input annotation and alignments
-    
+
     :return: either None type or an aligned annotation found in adjacent view
     """
     for al in view.get_annotations(AnnotationTypes.Alignment):
         if aligned_ann := ann.aligned_to_by(al):
             return aligned_ann
 
-def get_aligned_ann_of(mmif: Mmif, source: Annotation, source_app: str, target_app: str) -> Optional[Annotation]:
+def get_aligned_ann_of(
+    mmif: Mmif,
+    source: Annotation,
+    source_app: str,
+    target_app: str
+    ) -> Optional[Annotation]:
     """
     Get the aligned annotation of the input annotation cross views in MMIF
-    
+
     :param: mmif: the mmif file
     :param: source: the source annotation that looks for its aligned annotation in target view
     :param: source_app: the app that contains source annotation
-    :param: target_app: the app that might contain target aligned annotation 
-    
+    :param: target_app: the app that might contain target aligned annotation
+
     :return: either None type or an annotation
     """
-    valid_views = {view.metadata.app: view for view in mmif.views if not (view.has_error() or view.has_warnings())}
+    valid_views = {view.metadata.app: view
+                   for view in mmif.views if not (view.has_error() or view.has_warnings())}
 
     # Validate if two apps are in mmif
     if not (source_app and target_app) in valid_views:
         raise ValueError(f"Either {source_app} or {target_app} is not in mmif")
-    
+    #TODO: Think about more edge cases
     current_view, target_view = valid_views[source_app], valid_views[target_app]
     current_ann = source
     while current_view.id != target_view.id:
@@ -65,7 +72,7 @@ def get_aligned_ann_of(mmif: Mmif, source: Annotation, source_app: str, target_a
 def csv_string_to_pair(csv_string: str) -> Set[Tuple[str, str]]:
     """
     Convert csv-string to a set of pairs represeted by tuples
-    
+
     :param: csv_string: the csv-formatted string
     :return: a set of tuples of (role, filler) string
     """
@@ -74,22 +81,22 @@ def csv_string_to_pair(csv_string: str) -> Set[Tuple[str, str]]:
 def load_pred(file: Union[str, os.PathLike]) -> Dict[str, Dict]:
     """
     Load the predicted role-filler pair made by RFB
-    
+
     :param: file: the file path or name of the RFB MMIF
     :return: a nested dictionary data structure that indexes GUID -> frame_num -> (role, filler)
     """
     guid = guidhandler.get_aapb_guid_from(file)
-    
+
     rfb_mmif = Mmif(json.load(open(file, encoding='utf-8')))
     rfb_view = rfb_mmif.views.get_last_contentful_view()
-     
+
     frames_dict = {}
     for rfb_td in rfb_view.get_documents():
         aligned_tp = get_aligned_ann_of(rfb_mmif, rfb_td, RFB_APP, SWT_APP)
         aligned_frame = vdh.convert_timepoint(rfb_mmif, aligned_tp, 'frames')
         frames_dict[aligned_frame] = csv_string_to_pair(rfb_td.text_value)
-    
-    return {guid: frames_dict} 
+
+    return {guid: frames_dict}
 
 #--------------------------------------------------------------------
 # Functions needed to load gold standard data
@@ -97,9 +104,9 @@ def load_pred(file: Union[str, os.PathLike]) -> Dict[str, Dict]:
 def csv_string_to_set(csv_string: str) -> Set[Tuple[str, str]]:
     """
     Convert csv-string to a set of tuples which represent (role, filler) pairs
-    
+
     :params: csv_string: the input csv-formatted string
-    :return: a set of tuples of (role, filler) 
+    :return: a set of tuples of (role, filler)
     """
     rf_set = set()
     for pair in csv_string.split('\n'):
@@ -110,11 +117,11 @@ def csv_string_to_set(csv_string: str) -> Set[Tuple[str, str]]:
 def load_gold(gold_csv: Union[str, os.PathLike]) -> Dict[str, Dict]:
     """
     Load gold-standard csv data for RFB
-    
+
     As a review, its format looks like:
-    GUID | FRAME | SKIPPED | ANNOTATIONS
-    ------------------------------------
-    str  | int   |   T/F   | csv-string
+    GUID | FRAME | SWT-TYPE | SKIPPED | ANNOTATIONS
+    -----------------------------------------------
+    str  | int   |   str    |  T/F    | csv-string
     """
     guid = guidhandler.get_aapb_guid_from(gold_csv)
     frames_dict = defaultdict(set)
@@ -129,7 +136,7 @@ def load_gold(gold_csv: Union[str, os.PathLike]) -> Dict[str, Dict]:
 #--------------------------------------------------------------------
 class RFBMetrics:
     """
-    The class of evaluation metrics for evaluating RFB 
+    The class of evaluation metrics for evaluating RFB
     """
     @staticmethod
     def iou(
@@ -138,13 +145,13 @@ class RFBMetrics:
         key: str
     ) -> Tuple[str, float, Dict[int, float]]:
         """
-        Calculate the IOU value for a single frame on either role or filler 
+        Calculate the IOU value for a single frame on either role or filler
         as well as macro-avg of each frame's IOU value
-        
+
         :param: pred: the processed prediction data
         :param: gold: the processed gold data
         :param: key: the object to be calculated. here is either {role, filler}
-        :return: a tuple formatted as (macro-avg, {frame_num: iou}) 
+        :return: a tuple formatted as (macro-avg, {frame_num: iou})
         """
         def _iou(p, g):
             try:
@@ -156,11 +163,11 @@ class RFBMetrics:
         # Check if pred and gold data refer to the same video
         if list(pred.keys())[0] != list(gold.keys())[0]:
             raise ValueError('The prediction file and the gold file refer to different videos.')
-        
+
         # Find intersected frames between pred and gold
-        guid = next(iter(pred.keys()))  
+        guid = next(iter(pred.keys()))
         intersect_frames = set(pred[guid].keys(), gold[guid].keys())
-        
+
         # Calculate the iou
         obj_map = {'role':0, 'filler': 1}
         frame_score = {}
@@ -172,12 +179,17 @@ class RFBMetrics:
             frame_score[frame] = iou_val
             sum_of_iou += iou_val
 
-        return guid, sum_of_iou / len(intersect_frames), frame_score   
-                
+        return guid, sum_of_iou / len(intersect_frames), frame_score
+
 #--------------------------------------------------------------------
 # Write out evaluation results
 #--------------------------------------------------------------------
 def write_out(results: List[Tuple[str, float, Dict[int, float]]]) -> None:
+    """Write out a list of formatted evaluation results into a .txt
+
+    :param: results: the list of formatted evaluation results
+    :return: None
+    """
     with open('results.txt', 'w', encoding='utf-8') as f:
         for result in results:
             guid, macro_avg, frame_scores = result
@@ -191,51 +203,68 @@ def write_out(results: List[Tuple[str, float, Dict[int, float]]]) -> None:
 # Arguments setting
 #--------------------------------------------------------------------
 def parse_args() -> Namespace:
+    """Provide arguments of the script
+    """
     parser = ArgumentParser(
         description='Evaluation script for RFB (Role-Filler-Binding)'
     )
-    
+
     parser.add_argument(
         '-p',
         '--preds',
         help='The directory path of RFB predictions',
         required=True
     )
-    
+
     parser.add_argument(
         '-g',
         '--golds',
         help="The directory path of gold standard data",
         required=True
     )
-    
+
     return parser.parse_args()
 
 
 #--------------------------------------------------------------------
-# Main 
+# Main
 #--------------------------------------------------------------------
 def main():
+    """Main function for running the evaluation task for the RFB app
+    """
+    logging.basicConfig(filename='evaluate.log',
+                        format='%(message)s',
+                        filemode='w')
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
     args = parse_args()
     preds_dir, golds_dir = args.preds, args.golds
-    
+
+    logger.debug("Start loading gold-standard data:")
     golds = {}
     for root, _, gold_files in os.walk(golds_dir):
         for gold_file in gold_files:
+            log_msg = f"loading file {gold_file}..."
             golds.update(load_gold(os.path.join(root, gold_file)))
-            
+            logger.debug("\t%s DONE", log_msg)
+
+    logger.debug("\nStart loading predication data:")
     preds = {}
     for root, _, pred_files in os.walk(preds_dir):
         for pred_file in pred_files:
+            log_msg = f"\tloading file {pred_file}..."
             preds.update(load_pred(os.path.join(root, pred_file)))
-    
+            logger.debug("\t%s DONE", log_msg)
+
     overlap_videos = set(golds.keys() & preds.keys())
+    logger.debug("\nOverlap videos %s found", len(overlap_videos))
+
     iou_results = [RFBMetrics.iou(preds[guid], golds[guid], 'role') for guid in overlap_videos]
+
+    write_msg = '\nWriting out results...'
     write_out(iou_results)
+    logger.debug("%s Success!", write_msg)
 
 if __name__ == "__main__":
     main()
-        
-    
-     
-    
